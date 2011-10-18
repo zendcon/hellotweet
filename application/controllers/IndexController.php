2da0b807930960ef2a0570ef9e9d08a6d22e03e8
zendcon <productmanagement@zend.com>
2011-10-17 17:24:33 -0700

    Update application/controllers/IndexController.php

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,19 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
-    public function indexAction()
-    {
-        // action body
-    }
-
-
+class IndexController extends Zend_Controller_Action {
+    
+	public function init() {
+		/* Initialize action controller here */
+	}
+	
+	public function indexAction() {
+		// action body
+	}
+	
+	public function twitterAction() {
+		$twitterSearch = new Zend_Service_Twitter_Search ( 'json' );
+		$searchResults = $twitterSearch->search ( 'zendcon', array ('lang' => 'en' ) );
+		$this->view->results = $searchResults;
+	}
 }
 
